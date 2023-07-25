10b7354c7f2d289d35c0d5b9f36bb5c96685ed4d
luisbelisario <luisbls77@hotmail.com>
2023-07-25 10:18:53 -0300

    started client registration features

--- src/main/java/com/reservei/clientsapi/repository/AdminRepository.java
+++ src/main/java/com/reservei/clientsapi/repository/AdminRepository.java
@@ -2,6 +2,8 @@ package com.reservei.clientsapi.repository;
 
 import com.reservei.clientsapi.domain.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AdminRepository extends JpaRepository<Admin, Long> {
 }
--- src/main/java/com/reservei/clientsapi/repository/ClientRepository.java
+++ src/main/java/com/reservei/clientsapi/repository/ClientRepository.java
@@ -2,6 +2,8 @@ package com.reservei.clientsapi.repository;
 
 import com.reservei.clientsapi.domain.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 }
