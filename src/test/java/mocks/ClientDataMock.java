1c64f0e9b03db7d46c877d215146571c36ce645b
luisbelisario <luisbls77@hotmail.com>
2023-08-10 11:46:24 -0300

    preparing for integration deploy with security-api

--- src/test/java/mocks/ClientDataMock.java
+++ src/test/java/mocks/ClientDataMock.java
@@ -5,6 +5,6 @@ import com.reservei.clientsapi.domain.record.ClientData;
 public class ClientDataMock {
 
     public static ClientData getClientDataMock() {
-        return new ClientData("Cliente Teste", "teste@gmail.com", "00000000", "869954674894");
+        return new ClientData("Cliente Teste", "teste@gmail.com", "12345", "00000000000", "869954674894");
     }
 }
