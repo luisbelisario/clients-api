c95329fb7ce1ec701b1a84b14f087135f380a986
luisbelisario <luisbls77@hotmail.com>
2023-08-11 10:57:14 -0300

    preparing for deploy

--- src/test/java/mocks/ClientDtoMock.java
+++ src/test/java/mocks/ClientDtoMock.java
@@ -5,7 +5,7 @@ import com.reservei.clientsapi.domain.dto.ClientDto;
 public class ClientDtoMock {
 
     public static ClientDto getClientDtoMock() {
-        return new ClientDto(1L, "Cliente Teste",
+        return new ClientDto(1L, "12345", "Cliente Teste",
                 "teste@gmail.com", "00000000", "869954674894");
     }
 }
